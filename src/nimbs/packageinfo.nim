5418617a61a0594910f90a7d83c92bbf8f60621d
Anna “CyberTailor” <cyber@sysrq.in>
2022-06-28 16:23:43 +0500

    Implement populating the list of files to install

--- src/nimbs/packageinfo.nim
+++ src/nimbs/packageinfo.nim
@@ -2,9 +2,24 @@
 # SPDX-FileCopyrightText: 2022 Anna <cyber@sysrq.in>
 # SPDX-License-Identifier: BSD-3-Clause
 
-import os
+import logging, os, sequtils
 
 import nimbleexecutor, options
+
+type
+  PackageInfo* = object
+    nimbleFile*: string
+    name*: string
+    version*: string
+    requires*: seq[string]
+    installDirs*: seq[string]
+    installFiles*: seq[string]
+    installExt*: seq[string]
+    srcDir: string
+
+  InstallEntry* = tuple
+    kind: PathComponent
+    path: string
 
 proc getPackageName*(nimbleFile: string, options: Options): string =
   let packageName = nimbleFile.queryString("packageName", options)
@@ -25,3 +40,70 @@ proc findNimbleFile*(dir: string): string =
   elif hits == 0:
     quit("Could not find a file with a .nimble extension inside the " &
          "specified directory: " & dir)
+
+proc initPackageInfo*(options: Options): PackageInfo =
+  let nimbleFile = options.getSourceDir().findNimbleFile()
+  result = PackageInfo(
+    nimbleFile: nimbleFile,
+    name: nimbleFile.getPackageName(options),
+    version: nimbleFile.queryString("version", options),
+    requires: nimbleFile.queryArray("requiresData", options),
+    installDirs: nimbleFile.queryArray("installDirs", options),
+    installFiles: nimbleFile.queryArray("installFiles", options),
+    installExt: nimbleFile.queryArray("installExt", options),
+    srcDir: nimbleFile.queryString("srcDir", options)
+  )
+
+proc getSourceDir*(pkgInfo: PackageInfo, options: Options): string =
+  ## Returns the directory containing the package source files.
+  if pkgInfo.srcDir.len != 0:
+    return options.getSourceDir() / pkgInfo.srcDir
+  else:
+    return options.getSourceDir()
+
+proc getFilesWithExt(dir: string, installExt: seq[string]): seq[InstallEntry] =
+  for kind, path in walkDir(dir):
+    if kind == pcDir:
+      let contents = getFilesWithExt(path, installExt)
+      if contents.len != 0:
+        result.add (pcDir, path)
+        result &= contents
+    else:
+      if path.splitFile.ext.substr(1) in installExt:
+        result.add (pcFile, path)
+
+proc getFilesInDir(dir: string): seq[InstallEntry] =
+  for kind, path in walkDir(dir):
+    if kind == pcDir:
+      let contents = getFilesInDir(path)
+      if contents.len != 0:
+        result.add (pcDir, path)
+        result &= contents
+    else:
+      result.add (pcFile, path)
+
+proc getInstallFiles*(pkgInfo: PackageInfo, options: Options): seq[InstallEntry] =
+  let sourceDir = pkgInfo.getSourceDir(options)
+  let whitelistMode =
+          pkgInfo.installDirs.len != 0 or
+          pkgInfo.installFiles.len != 0 or
+          pkgInfo.installExt.len != 0
+  if whitelistMode:
+    for file in pkgInfo.installFiles:
+      let src = sourceDir / file
+      if not fileExists(src):
+        warn("Missing file " & src)
+        continue
+      result.add (pcFile, src)
+
+    for dir in pkgInfo.installDirs:
+      let src = sourceDir / dir
+      if not dirExists(src):
+        warn("Missing directory " & src)
+        continue
+      result.add (pcDir, src)
+      result &= getFilesInDir(src)
+
+    result &= getFilesWithExt(sourceDir, pkgInfo.installExt)
+
+  return deduplicate(result)
