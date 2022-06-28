923b640e794316cc64cf41f69922f9baa9e1fb94
Anna “CyberTailor” <cyber@sysrq.in>
2022-06-29 03:45:38 +0500

    Implement setup action

--- src/nimbs/common.nim
+++ src/nimbs/common.nim
@@ -2,7 +2,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 const
+  nimbusVersion* = "0.1.0"
+
   defaultNimbleDir* = "/opt/nimble"
   nimblePackagesDirName* = "pkgs2"
 
   packageMetadataFileName* = "nimblemeta.json"
+
+  installerFileName* = "installer.nims"
